bd07d996c7b4f3c2c4bed7afa3f602e74022d557
Hunchogift <giftmasosote@gmail.com>
2025-04-18 22:13:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Caseyrhodes~oaEUkJZS#w8xp6CB7-dQ7fl2jkqf4Prz72muin_evp1QBvvF3QuQ",
+SESSION_ID: process.env.SESSION_ID || "Caseyrhodes~gfMRkSxB#4UVZoWJclQtPgJgjKLuMxiXxPuReQQgUmy9Oy4QCb3c",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -34,31 +34,9 @@ AUTO_REACT: process.env.AUTO_REACT || "true",
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*🎉👀 Seen by Gift_Masosote*",
-// set the auto reply massage on status reply    
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-HEART_REACT: process.env.HEART_REACT || "false",
-// make this true or false for heart reactions only 
-OWNER_REACT: process.env.OWNER_REACT || "true",
-// make it true or fasle for only react on owner msg only 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
-// maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-// make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "true",
-// true for automatic show typing   
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
-};
+
+
+
+
+
+
